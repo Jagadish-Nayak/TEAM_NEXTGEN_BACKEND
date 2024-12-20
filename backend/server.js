07f2fe102a27283f500b6cca54234f3994ade74c
Jagadish-Nayak <122447489+Jagadish-Nayak@users.noreply.github.com>
2024-12-20 19:28:49 +0530

    Updated server.js

--- backend/server.js
+++ backend/server.js
@@ -24,7 +24,7 @@ app.use(cookieParser());
 // Configure CORS
 app.use(
     cors({
-        origin: 'http://localhost:3000', // Your frontend URL
+        origin: 'https://team-nextgen.netlify.app', // Your frontend URL
         credentials: true, // Allow credentials (cookies)
         methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
         allowedHeaders: ['Content-Type', 'Authorization'],
